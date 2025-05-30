d95846ffa37b72d81b77d08292db74c4d07b724c
Silvano Cerza <silvanocerza@gmail.com>
2025-05-30 11:18:32 +0200

    Add /health_check endpoint and relative tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let ip = "127.0.0.1";
+    let listener = TcpListener::bind(format!("{}:0", ip)).expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Running server on http://{}:{}", ip, port);
+    run(listener)?.await
 }
